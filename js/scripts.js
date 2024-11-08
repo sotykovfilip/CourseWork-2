b62a3ea7a2302f75a04808fc22955439e9500ffa
Filip Stoykov <midgetgamer29@gmail.com>
2024-11-08 13:11:38 +0000

    Next

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,71 @@
 function calculateMortgage() {
-    const loanAmount = parseFloat(document.getElementById('loanAmount').value);
-    const loanTerm = parseInt(document.getElementById('loanTerm').value) * 12;
-    const monthlyIncome = parseFloat(document.getElementById('monthlyIncome').value);
+  const loanAmount = parseFloat(document.getElementById("loanAmount").value);
+  const loanTerm = parseInt(document.getElementById("loanTerm").value) * 12;
+  const monthlyIncome = parseFloat(
+    document.getElementById("monthlyIncome").value
+  );
 
-    const interestRate = 0.045 / 12;
-    const monthlyPayment = (loanAmount * interestRate * Math.pow(1 + interestRate, loanTerm)) / (Math.pow(1 + interestRate, loanTerm) - 1);
+  const interestRate = 0.045 / 12;
+  const monthlyPayment =
+    (loanAmount * interestRate * Math.pow(1 + interestRate, loanTerm)) /
+    (Math.pow(1 + interestRate, loanTerm) - 1);
 
-    const resultDiv = document.getElementById('result');
-    const infoDiv = document.getElementById('resultInfo');
-    const br = document.createElement('br');
-    resultDiv.classList.remove('success', 'error');
-    infoDiv.innerText = '';
+  const resultDiv = document.getElementById("result");
+  const infoDiv = document.getElementById("resultInfo");
+  const br = document.createElement("br");
+  resultDiv.classList.remove("success", "error");
+  infoDiv.innerText = "";
 
-    if (monthlyPayment > 0.3 * monthlyIncome) {
-        resultDiv.innerText = "Loan amount too high based on your monthly income.";
-        resultDiv.classList.add('error');
-        let maxMonthly = 0.3 * monthlyIncome;
-        let maxLoan = (maxMonthly / interestRate) * (Math.pow(1 + interestRate, loanTerm) - 1) / Math.pow(1 + interestRate, loanTerm);
-        infoDiv.innerHTML = `The maximum loan amount you can afford for a ${loanTerm} month long loan is £${maxLoan.toFixed(2)}`;
-    } else {
-        resultDiv.innerText = `Your monthly payment is: £${monthlyPayment.toFixed(2)}`;
-        infoDiv.innerText = `Based on a loan amount of £${loanAmount.toFixed(2)} over ${loanTerm / 12} years at 4.5% interest rate.`;
-        infoDiv.appendChild(br);
-        infoDiv.innerHTML += `The total amount paid will be £${(monthlyPayment * loanTerm).toFixed(2)}`;
-        resultDiv.classList.add('success');
-    }
+  if (monthlyPayment > 0.3 * monthlyIncome) {
+    resultDiv.innerText = "Loan amount too high based on your monthly income.";
+    resultDiv.classList.add("error");
+    let maxMonthly = 0.3 * monthlyIncome;
+    let maxLoan =
+      ((maxMonthly / interestRate) *
+        (Math.pow(1 + interestRate, loanTerm) - 1)) /
+      Math.pow(1 + interestRate, loanTerm);
+    infoDiv.innerHTML = `The maximum loan amount you can afford for a ${loanTerm} month long loan is £${maxLoan.toFixed(
+      2
+    )}`;
+  } else {
+    resultDiv.innerText = `Your monthly payment is: £${monthlyPayment.toFixed(
+      2
+    )}`;
+    infoDiv.innerText = `Based on a loan amount of £${loanAmount.toFixed(
+      2
+    )} over ${loanTerm / 12} years at 4.5% interest rate.`;
+    infoDiv.appendChild(br);
+    infoDiv.innerHTML += `The total amount paid will be £${(
+      monthlyPayment * loanTerm
+    ).toFixed(2)}`;
+    resultDiv.classList.add("success");
+  }
 
-    resultDiv.style.display = 'block';
-
+  resultDiv.style.display = "block";
 }
 
-$(document).ready(function() {
-    $('.accordion-header').on('click', function() {
-        $(this).next('.accordion-content').stop(true, true).slideToggle();
-        $('.accordion-header').not(this).removeClass('active');
-        $(this).toggleClass('active');
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      } else {
+        entry.target.classList.remove("show");
+      }
     });
+  });
+  
+const hiddenElements = document.querySelectorAll(".hidden");
+
+hiddenElements.forEach((element) => {
+    observer.observe(element);
 });
+  
 
-(function () {
-    "use strict";
 
-    const COMPONENT_ID = 'scroll'; 
-
-    var scrollButton = document.getElementById(COMPONENT_ID);
-
-    $(scrollButton).on('click', function (event) {
-        event.preventDefault();
-
-        $('html, body').animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
-        }, 500);
-    });
-})()
+$(document).ready(function () {
+  $(".accordion-header").on("click", function () {
+    $(this).next(".accordion-content").stop(true, true).slideToggle();
+    $(".accordion-header").not(this).removeClass("active");
+    $(this).toggleClass("active");
+  });
+});
