2dbbf39a009ea0d6cf3139b01873872b3153787e
Filip Stoykov <midgetgamer29@gmail.com>
2024-11-15 11:48:58 +0000

    Add breakdown button to mortgage calculator and update result display logic

--- js/scripts.js
+++ js/scripts.js
@@ -11,30 +11,37 @@ function calculateMortgage() {
     (Math.pow(1 + interestRate, loanTerm) - 1);
 
   const resultDiv = document.getElementById("result");
-  const infoDiv = document.getElementById("resultInfo");
+  //const infoDiv = document.getElementById("resultInfo");
   const br = document.createElement("br");
+  const bdButton = document.getElementById("breakDownButton");
   resultDiv.classList.remove("success", "error");
-  infoDiv.innerText = "";
+  //infoDiv.innerText = "";
 
-  if (monthlyPayment > 0.3 * monthlyIncome) {
-    resultDiv.innerText = "Loan amount too high based on your monthly income.";
-    resultDiv.classList.add("error");
+  if ( monthlyIncome < 0.3 * monthlyIncome) {
+    resultDiv.innerHTML = `Your monthly payment is: £${monthlyPayment.toFixed(2)}`;
+    resultDiv.appendChild(br);
+    resultDiv.innerHTML += `That equates to a total of: £${(monthlyPayment * loanTerm).toFixed(2)}`;
+    resultDiv.classList.add("success");
+    bdButton.style.display = "block";
+  } else if (monthlyPayment > 0.3 * monthlyIncome) {
+    //resultDiv.innerText = "Loan amount too high based on your monthly income.";
+    //  resultDiv.classList.add("error");
     let maxMonthly = 0.3 * monthlyIncome;
     let maxLoan =((maxMonthly / interestRate) *(Math.pow(1 + interestRate, loanTerm) - 1)) /Math.pow(1 + interestRate, loanTerm);
     infoDiv.innerHTML = `The maximum loan amount you can afford for a ${loanTerm} month long loan is £${maxLoan.toFixed(2)}`;
   } else {
-    resultDiv.innerHTML = `Your monthly payment is: £${monthlyPayment.toFixed(2)}`;
-    resultDiv.appendChild(br);
-    resultDiv.innerHTML += `That equates to a total of: £${(monthlyPayment * loanTerm).toFixed(2)}`;
-    infoDiv.innerHTML = '<h3>Break Down</h3>';
-    infoDiv.appendChild(br);
-    infoDiv.innerHTML += `Based on a loan amount of £${loanAmount.toFixed(2)} over ${loanTerm / 12} years at 4.5% interest rate.`;
-    infoDiv.appendChild(br);
-    infoDiv.innerHTML += `The total amount paid will be £${(monthlyPayment * loanTerm).toFixed(2)}`;
-    resultDiv.classList.add("success");
+    resultDiv.innerText = "Please enter valid values.";
+    resultDiv.classList.add("error");
   }
 
   resultDiv.style.display = "block";
+}
+
+function breakDownButton(){
+  const breakDown = document.getElementById("breakDown");
+  const breakDownDiv = document.getElementById("breakDownDiv");
+  breakDownDiv.style.display = "block";
+  breakDown.style.display = "none";
 }
 
 const observer = new IntersectionObserver((entries) => {
